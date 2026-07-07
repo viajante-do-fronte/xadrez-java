09c8ef00df69fff4a03821fcc16e67afc0fc2e7f
Enzo Nascimento <enzofanart@gmail.com>
2026-07-07 13:21:43 -0300

    imprimindo o tabuleiro

--- src/aplication/programa.java
+++ src/aplication/programa.java
@@ -1,16 +1,16 @@
 package aplication;
 
-import tabuleiro.Posicao;
+import xadrez.PartidaXadrez;
 
 public class programa {
 	
 	 public static void main(String[] args) {
 		 
-		 Posicao posição = new Posicao(3, 5);
-		 System.out.println(posição);
+		 PartidaXadrez partidaXadrez = new PartidaXadrez();
+		 	IU.printTabuleiro(partidaXadrez.getPecas());
 		
 	}
-
-	}
+	 
+}
 
 
--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,5 +1,39 @@
 package tabuleiro;
 
 public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+    private Peca[][] pecas;
+    
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		pecas = new Peca[linhas][colunas];
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	public Peca peça(int linha, int coluna) {
+		return pecas[linha][coluna];
+	}
+	
+	public Peca peça (Posicao posicao) {
+		return pecas[posicao.getLinha()][posicao.getColuna()];	
+	}
 
 }
